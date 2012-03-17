40bdcee561ce79cf4f26640bf1922d0821cb7f18
mantera <som4th@gmail.com>
2012-03-17 13:23:26 -0400

    fix up

--- include/linux/libra_sdioif.h
+++ include/linux/libra_sdioif.h
@@ -82,8 +82,6 @@ int libra_sdio_configure_suspend_resume(
 		resume_handler_t *libra_sdio_resume_hdlr);
 
 int libra_detect_card_change(void);
-void libra_sdio_set_clock(struct sdio_func *func, unsigned int clk_freq);
-void libra_sdio_get_card_id(struct sdio_func *func, unsigned short *card_id);
 void libra_sdio_release_irq(struct sdio_func *func);
 void libra_sdio_disable_func(struct sdio_func *func);
 
